77697f1e6ba8c931ab59be2602c7e2634e6ecf08
george-tr6 <george.tr6@gmail.com>
2026-05-14 14:35:53 -0700

    Added comments with the steps needed

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 console.log("Hello World");
+
+//Write the logic to get the computer choice
+
+//Write the logic to get the human choice
+
+//Declare the players score variables
+
+//Write the logic to play a single round
+
+//Write the logic to play the entire game
