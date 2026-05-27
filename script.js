12c5163c51fcd802affff390faec9c5839d58b44
george-tr6 <george.tr6@gmail.com>
2026-05-27 11:00:32 -0700

    Created the getComputerChoice function will  randomly return one of the following string   values: rock, paper or scissors

--- script.js
+++ script.js
@@ -2,6 +2,30 @@ console.log("Hello World");
 
 //Write the logic to get the computer choice
 
+function getComputerChoice() {
+
+    let randomnumber = Math.random() * 10;
+    let computerchoice = Number(randomnumber);
+    let label;
+
+    if (computerchoice <= 3.333) {
+        // console.log(computerchoice);
+        label = "Rock";
+
+    } else if (computerchoice > 3.333 && computerchoice <= 6.667) {
+        // console.log(computerchoice);
+        label = "Paper";
+
+    } else {
+        // console.log(computerchoice);
+        label = "Scissors";
+    };
+
+    finalchoice = label.toLocaleLowerCase();
+    return finalchoice;
+}
+
+
 //Write the logic to get the human choice
 
 //Declare the players score variables
